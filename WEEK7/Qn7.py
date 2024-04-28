44a75a66bcfdc5538d8b6da1786d9f3fc9c12aba
Sreelakshmi <sreeluvkv@gmail.com>
2024-04-29 01:10:02 +0530

    meh

--- WEEK7/Qn7.py
+++ WEEK7/Qn7.py
@@ -3,7 +3,7 @@ import random
 import numpy as np
 import scipy.integrate as integrate 
 import matplotlib.pyplot as plt
-def poly(n, f,plot = False, a = 0, b = np.pi):
+def poly(n, f,M,plot = False, a = 0, b = np.pi):
     
     A = []
     B = []
@@ -21,8 +21,8 @@ def poly(n, f,plot = False, a = 0, b = np.pi):
     P = Polynomial([0])
     P.coeff = np.array([float(i) for i in X])
     if plot : 
-        P.show(a,b)
-        x_coords = np.linspace(a,b,100)
+        P.show(a,b) 
+        x_coords = np.linspace(a,b,M)
         y_coords = f(x_coords)
         plt.plot(x_coords,y_coords,label = 'True', color= 'red')
         plt.show()
@@ -32,4 +32,23 @@ def roots (f,a,b):
     M = int((b-a)*100)
     x_prev = a
     X0 = []
-    
+    for x in np.linspace(a,b,M):
+        if f(x_prev) * f(x) <= 0:
+            X0.append(x)
+        x_prev = x
+
+    P = poly(len(X0)+5,f,M,a=a,b=b)
+    return P.printRoots(X0)
+
+
+if __name__ == '__main__':
+
+
+    print("x³ - 6x² + 11x - 6")
+    print("Intervel [-5, 5]")
+    f = lambda x: x**3 - 6*x**2 + 11*x - 6
+    X = roots(f, -5, 5)
+    X_actual = [1, 2, 3]
+    print("calculated roots :", X)
+    print("actual roots:", X_actual)
+    print("f(X): ", [f(x) for x in X])
--- WEEK7/polynomial.py
+++ WEEK7/polynomial.py
@@ -148,3 +148,30 @@ class Polynomial:
         for i in range(1,len(coeff)):
             newCoeff.append( i*coeff[i])
         return Polynomial(newCoeff)
+
+def aberth_method(P:Polynomial, e , Z):
+    """Z is the initial guesses
+    """
+    n = min(len(P.coeff)-1, len(Z))
+
+    if n == 0 :
+        return None
+    
+    Z0 = [z+10*e for z in Z]
+
+    while any([abs(Z[i] - Z0[i]) > e for i in range(n)]):
+        """
+        for all k
+        z_k(t+1) = z_k(t) - 1/(P'(z_k(t))/P(z_k(t)) - sum(z_k - z_j for all j, k != j))
+
+        halts when change in each root estimate is less than ɛ.
+        """
+        memo = [sum([1/(Z[k]-Z[j]) if k!=j else 0 for j in range(n)]) for k in range(n)]
+        Z0 = Z.copy()
+        for i in range(n):
+            t = P(Z[i])/P.derivative()(Z[i])
+            Z[i] -= t / (1-t*memo[i])
+
+        return Z
+
+Polynomial.printRoots = lambda self,Z0 : aberth_method(self,10e-3,Z0)
